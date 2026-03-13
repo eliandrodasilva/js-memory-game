02cc58f837625e037c7903c3cf4b032398eb3204
Eliandro Gomes <eliandro.gomes42@gmail.com>
2026-03-13 15:12:25 -0300

    feat: add attempts tracking to the memory game

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ import { searchWords, saveGame } from "./api.js";
 import { shuffle } from "./utils.js";
 
 const CARDS = document.querySelectorAll(".card");
+const ATTEMPTS = document.getElementById("attempts");
 
 let firstCardSelected = null;
 let secondCardSelected = null;
@@ -9,6 +10,7 @@ let attempts = 0;
 let isLocked = false;
 
 export async function startGame() {
+  resetAttempts();
   let words = await searchWords();
   let shuffledCards = shuffle([...words, ...words]);
   CARDS.forEach((card, index) => {
@@ -32,7 +34,7 @@ function flipCard(card) {
   }
 
   secondCardSelected = card;
-  attempts++;
+  updateAttempts();
   compareCards();
 }
 
@@ -56,4 +58,14 @@ function compareCards() {
       isLocked = false;
     }, 600);
   }
+}
+
+function updateAttempts() {
+  attempts++;
+  ATTEMPTS.textContent = attempts
+}
+
+function resetAttempts() {
+  ATTEMPTS.textContent = "0"
+  attempts = 0;
 }
